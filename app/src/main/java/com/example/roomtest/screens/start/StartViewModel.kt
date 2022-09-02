a2d798258465b02507daad22ae9bc742baa3e41b
kabanov871 <110372567+kabanov871@users.noreply.github.com>
2022-09-02 09:36:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/roomtest/screens/start/StartViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.roomtest.screens.start
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.roomtest.REPOSITORY
+import com.example.roomtest.db.NoteDatabase
+import com.example.roomtest.db.repository.NoteRealization
+import com.example.roomtest.model.NoteModel
+
+class StartViewModel(application: Application): AndroidViewModel(application) {
+
+    val context = application
+    fun initDatabase(){
+        val daoNote = NoteDatabase.getInstance(context).getNoteDao()
+        REPOSITORY = NoteRealization(daoNote)
+    }
+    fun getAllNotes(): LiveData<List<NoteModel>>{
+        return REPOSITORY.allNotes
+
+    }
+}
